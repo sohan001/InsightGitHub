982fa1c4d5404ed9b22282beae97cacf2276d1a6
sohan001 <sohan.singh@decurtis.com>
2015-12-29 17:52:22 +0530

    Initiallly checking solution

---
+++ InsightGitHub/InsightGitHub/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace InsightGitHub.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ InsightGitHub/InsightGitHub/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace InsightGitHub.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
