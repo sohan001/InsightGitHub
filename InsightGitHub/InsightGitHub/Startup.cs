70396329f3198e773a1f9daac4b714bea9f1fdbb
deepakb100 <deepak.bhatia@decurtis.com>
2015-12-29 18:11:06 +0530

    Update Startup.cs

--- InsightGitHub/InsightGitHub/Startup.cs
+++ InsightGitHub/InsightGitHub/Startup.cs
@@ -15,6 +15,7 @@ namespace InsightGitHub
             ConfigureAuth(app);
             // Making some changes on code in Master
             // This line is edited by  sohan's account
+            // This line is edited by  deepak's account
         }
     }
 }
